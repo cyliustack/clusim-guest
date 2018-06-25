fb893a3b8a3907034d50c6422270e1c49421a4e8
Your Name <you@example.com>
2018-06-25 14:00:56 +0800

    update installing mpich and benchmarks

--- prepare.sh
+++ prepare.sh
@@ -32,13 +32,37 @@ function inform_sudo()
     [[ $? != 0 ]] && >&2 echo -e "\033[1;31mAbort\033[0m" && exit 1;
 }
 
+function install_benchmarks()
+{
+	# Install Python packages
+	echo -e "${C_GREEN}Installing benchmarks...${C_NONE}"
+	mpich_version=3.2
+	mpich_package=mpich-$mpich_version
+	mpich_tar=$mpich_package.tar.gz
+	#openmpi_version=openmpi-3.1.0
+	#openmpi_tar=$openmpi_version.tar.gz
+	if [[ ! -f $mpich_tar ]] ; then
+		#wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.0.tar.gz
+		wget http://www.mpich.org/static/downloads/$mpich_version/$mpich_tar
+		echo "mpich is downloaded" 
+	fi
+	tar xvf $mpich_tar
+	pushd $mpich_package && ./configure --prefix=/opt/mpich && make -j && sudo make install && popd 
+ 	if [[ $(which mpirun) ]]; then	
+		echo export PATH=/opt/mpich/bin:$PATH >> ~/.bashrc
+ 		echo export LD_LIBRARY_PATH=/opt/mpich/lib:$LD_LIBRARY_PATH >> ~/.bashrc
+	fi
+	[[ $? != 0 ]] && echo -e "${C_RED_BK}Failed... :(${C_NONE}" && exit 1
+}
+
+
 function install_python_packages()
 {
     # Install Python packages
     echo -e "${C_GREEN}Installing python packages...${C_NONE}"
     python3 -m pip install --upgrade pip
-    python3 -m pip install numpy  
-    python3 -m pip install tensorflow==1.5.0 
+    python3 -m pip install --user numpy  
+    python3 -m pip install --user tensorflow==1.5.0 
     [[ $? != 0 ]] && echo -e "${C_RED_BK}Failed... :(${C_NONE}" && exit 1
 }
 
@@ -48,13 +72,13 @@ function install_packages()
 
     #inform_sudo "Running sudo for installing packages"
     if [[ $(which apt) ]] ; then
-        apt-get update
-        apt-get update --fix-missing
-	apt-get install -y gcc g++ curl wget python3 python3-pip
+        sudo apt-get update
+        sudo apt-get update --fix-missing
+	sudo apt-get install -y gcc g++ curl wget python3 python3-pip gfortran
 	[[ $? != 0 ]] && echo -e "${C_RED_BK}Failed... :(${C_NONE}" && exit 1
     elif [[ $(which yum) ]]  ; then
-        yum install -y epel-release 
-        yum install -y centos-release-scl devtoolset-4-gcc* python3 python-pip
+        sudo yum install -y epel-release 
+        sudo yum install -y centos-release-scl devtoolset-4-gcc* python3 python-pip
         [[ $? != 0 ]] && echo -e "${C_RED_BK}Failed... :(${C_NONE}" && exit 1
     else
         echo -e "${C_RED_BK}This script does not support your OS distribution, '$OS'. Please install the required packages by yourself. :(${C_NONE}"
@@ -68,5 +92,5 @@ printf "\n\n"
 
 install_packages
 install_python_packages
-
+install_benchmarks
 echo -e "${C_GREEN}Complete!!${C_NONE}"
